c765a837d5ccc6f70c3deb95d182a3e29084ecf6
Claudio Martins <claudio@cinbesa.com.br>
2024-09-12 17:29:20 -0300

    Segundo commit Testes

--- aulasDS/src/main/java/ifpa/aulasds/AulasDS.java
+++ aulasDS/src/main/java/ifpa/aulasds/AulasDS.java
@@ -3,6 +3,6 @@ package ifpa.aulasds;
 public class AulasDS {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World, IFPA!");
     }
 }
